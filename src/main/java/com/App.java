e9c1700bdb785bdb2cf1de009acb3d4d28d1d5f8
Dhruvik <dhruviksparikh@gmail.com>
2022-11-13 19:51:02 +0530

    Added changes

---
+++ src/main/java/com/App.java
@@ -0,0 +1,22 @@
+package com;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.model.Employee;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("appContext.xml");
+
+		Employee e = (Employee) appContext.getBean("e1");
+		System.out.println(e.getFirstName() + " " + e.getLastName() + " " + e.getAddress().getCity() + " "
+				+ e.getAddress().getState() + " " + e.getAddress().getCountry());
+	
+	
+	}
+	
+
+}
